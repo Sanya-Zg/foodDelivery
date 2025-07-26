36080611cc6b1ea56453278e956c5904a91b1fe7
Oleksandr Mas <sanya.maslovskii@gmail.com>
2025-07-26 06:56:10 +0300

    created login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,120 @@
+import { useState } from 'react';
+import { FaRegEyeSlash } from 'react-icons/fa6';
+import { FaRegEye } from 'react-icons/fa6';
+import toast from 'react-hot-toast';
+import SummaryApi from '../common/summaryApi';
+import Axios from '../utils/Axios';
+import AxiosToastError from '../utils/AxiosToastError';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Login = () => {
+  const [data, setData] = useState({
+    email: '',
+    password: '',
+  });
+  const [showPassword, setShowPassword] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setData((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
+
+  const valideValue = Object.values(data).every((el) => el);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await Axios({
+        ...SummaryApi.login,
+        data: data,
+      });
+
+      if (response.data.error) {
+        toast.error(response.data.message);
+      }
+
+      if (response.data.success) {
+        toast.success(response.data.message);
+
+        setData({
+          email: '',
+          password: '',
+        });
+        navigate('/');
+      }
+      console.log('response', response);
+    } catch (error) {
+      AxiosToastError(error);
+    }
+  };
   return (
-    <div>Login</div>
-  )
-}
-export default Login
+    <section className="w-full container mx-auto px-2">
+      <div className="bg-white my-4 w-full max-w-lg mx-auto rounded p-7">
+        <p className='font-bold text-2xl'>Log in</p>
+
+        <form className="grid gap-2 mt-6" onSubmit={handleSubmit}>
+          
+          <div className="grid gap-1">
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              className="bg-blue-50 p-2 border rounded outline-none focus:border-primary-200"
+              value={data.email}
+              name="email"
+              onChange={handleChange}
+              placeholder="Enter your email"
+            />
+          </div>
+          <div className="grid gap-1">
+            <label htmlFor="password">Password:</label>
+            <div className="bg-blue-50 p-2 border rounded flex items-center focus-within:border-primary-200">
+              <input
+                type={showPassword ? 'text' : 'password'}
+                id="password"
+                className="w-full outline-none"
+                value={data.password}
+                name="password"
+                onChange={handleChange}
+                placeholder="Enter your password"
+              />
+              <div
+                onClick={() => setShowPassword((prev) => !prev)}
+                className="cursor-default"
+              >
+                {showPassword ? <FaRegEye /> : <FaRegEyeSlash />}
+              </div>
+            </div>
+          </div>
+          
+          <button
+            disabled={!valideValue}
+            className={`${
+              valideValue ? 'bg-green-800 hover:bg-green-700' : 'bg-gray-500'
+            }  text-white py-2 rounded font-semibold my-3 tracking-wide`}
+          >
+            Login
+          </button>
+        </form>
+
+        <p>
+          Don't have account?{' '}
+          <Link
+            to={'/register'}
+            className="font-semibold text-green-700 hover:text-green-800"
+          >
+            Register
+          </Link>
+        </p>
+      </div>
+    </section>
+  );
+};
+export default Login;
